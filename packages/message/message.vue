3079daab4149393a0a70d8db81d69bd0b48691c5
kinggq <18701094030@163.com>
2023-08-01 22:29:07 +0800

    chore: update

--- packages/message/message.vue
+++ packages/message/message.vue
@@ -1,10 +1,48 @@
 <script setup lang="ts">
-import type { Contact } from 'packages'
+import type { Contact, Message, MessageStatus, UserInfo } from 'packages'
+
+const emits = defineEmits<{
+  (e: 'pullMessage', next: () => void): void
+}>()
 
 defineOptions({
   name: 'NcMessage',
 })
-const activeMessage = inject<Contact>('active-message')
+
+const userInfo = inject<ComputedRef<UserInfo>>('user-info')
+const currentContact = inject<Ref<Contact>>('current-contact')!
+
+const currentMessage = inject<Ref<MessageStatus>>('current-message')!
+
+function getContentClass(content: Message) {
+  if (content.fromUser.id === userInfo?.value.id)
+    return 'flex flex-row-reverse'
+  return 'flex flex-justify-start'
+}
+
+function onScroll(event: Event) {
+  const el = event.target as HTMLElement
+
+  if (currentMessage.value.isEnd)
+    return
+  if (el.scrollTop === 0) {
+    // console.log('onScroll')
+    currentMessage.value.loading = true
+    emits('pullMessage', () => {
+      currentMessage.value.loading = false
+    })
+  }
+}
+
+const scrollContainer = ref()
+
+async function scrollToBottom() {
+  await nextTick()
+  if (scrollContainer.value)
+    scrollContainer.value.scrollTop = scrollContainer.value.scrollHeight
+}
+
+defineExpose({ scrollToBottom })
 </script>
 
 <template>
@@ -18,14 +56,45 @@ const activeMessage = inject<Contact>('active-message')
         font-500
         text="14px gray-500/100"
       >
-        {{ activeMessage?.nickname }}
+        {{ currentContact?.nickname }}
       </div>
       <div i-ri:more-line />
     </div>
-    <div flex="~ 1" />
+    <div
+      ref="scrollContainer"
+      flex-1
+      overflow-auto px-15px
+      py-10px
+      @scroll="onScroll"
+    >
+      <div v-if="currentMessage?.loading">
+        Loading...
+      </div>
+      <div
+        v-for="item in (currentMessage ?? {}).data"
+        :key="item.id"
+        w-full py-10px
+      >
+        <div :class="getContentClass(item)">
+          <div>
+            <img h-36px w-36px rounded-4px :src="item.fromUser.avatar" alt="">
+          </div>
+          <div
+            text="14px"
+            flex="~ items-center"
+            bg="green-500/60"
+            ml-10px mr-10px
+            rounded-6px
+            px-10px py-5px
+          >
+            {{ item.content }}
+          </div>
+        </div>
+      </div>
+    </div>
     <div
       border-t="1px gray-500/10"
-      h-200px
+      min-h-200px
     />
   </div>
 </template>
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { Contact } from '../packages'
+import type { Contact, Message, PullMessageOption } from '../packages'
 import { NaiveChat } from '../packages'
 
+const userInfo = {
+  nickname: 'King',
+  avatar: 'https://thirdwx.qlogo.cn/mmopen/vi_32/RMksZlPP4myx9pbGzt3PmV2FNIpia8hVHpUXbHM0RfbJtsSMEWCLicbvGuJRMpoAam3sZclNo0YtOnvJ0a8eMtyQ/132',
+  id: 1,
+}
 const contacts = ref<Contact[]>([])
 
 for (let i = 0; i < 10; i++) {
   contacts.value.push({
-    id: i,
+    id: i + 1,
     nickname: `好友${1 + i}`,
     avatar: 'https://thirdwx.qlogo.cn/mmopen/vi_32/RMksZlPP4myx9pbGzt3PmV2FNIpia8hVHpUXbHM0RfbJtsSMEWCLicbvGuJRMpoAam3sZclNo0YtOnvJ0a8eMtyQ/132',
     lastMessage: 'hello',
@@ -17,11 +22,52 @@ for (let i = 0; i < 10; i++) {
 // console.log('1')
 function changeContact(contact: Contact) {
   // console.log('组件', contact)
+  Boolean(contact.avatar)
+}
+
+function pullMessage({ next, contactId }: PullMessageOption) {
+  // console.log('pullMessage')
+  if (contactId === 1) {
+    const messages: Message[] = []
+
+    for (let i = 0; i < 20; i++) {
+      messages.push({
+        id: `${i}`,
+        content: `消息${i + 1}`,
+        type: 'text',
+        fromUser: {
+          id: i % 2 === 0 ? 1 : 100 + i,
+          avatar: i % 2 === 0
+            ? 'https://thirdwx.qlogo.cn/mmopen/vi_32/RMksZlPP4myx9pbGzt3PmV2FNIpia8hVHpUXbHM0RfbJtsSMEWCLicbvGuJRMpoAam3sZclNo0YtOnvJ0a8eMtyQ/132'
+            : 'https://img2.baidu.com/it/u=3681172266,4264167375&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1690995600&t=1cff4e7d456c4118076598b7c03fe190',
+        },
+      })
+    }
+    asyncFn(() => {
+      next(messages)
+    })
+  }
+  else {
+    asyncFn(() => {
+      next([])
+    })
+  }
+}
+
+function asyncFn(fn: () => void) {
+  setTimeout(() => {
+    fn()
+  }, 3000)
 }
 </script>
 
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <NaiveChat :contacts="contacts" @change-contact="changeContact" />
+    <NaiveChat
+      :user-info="userInfo"
+      :contacts="contacts"
+      @change-contact="changeContact"
+      @pull-message="pullMessage"
+    />
   </main>
 </template>
